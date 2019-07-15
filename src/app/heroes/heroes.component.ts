0c2352b44372b73659eaae8ee915c157068a3cd3
Erick Tepale <e.tepale@gmail.com>
2019-07-15 18:12:09 -0400

    Finished part 4
    Going to start part 5

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../classes/hero';
+import { HeroService } from '../services/hero.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { 
+  
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero:Hero):void{
+    this.selectedHero = hero;
+  }
+
+  /**
+   * The new version waits for the Observable to emit the 
+   * array of heroes— which could happen now or several 
+   * minutes from now. Then subscribe passes the emitted 
+   * array to the callback, which sets the component's 
+   * heroes property.
+   */
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+}
---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '../classes/hero';
+import { HEROES } from '../mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  //inject MessageSerivce to HeroService.
+  //Then HeroService gets injected into HeroesComponent
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
